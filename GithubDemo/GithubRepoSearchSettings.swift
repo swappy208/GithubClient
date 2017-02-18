c5da3156f256cb4542554b22f5fbb812812c2fb7
swappy208@gmail.com <swappy208@gmail.com>
2017-02-18 13:13:37 -0500

    Added my files

---
+++ GithubDemo/GithubRepoSearchSettings.swift
@@ -0,0 +1,19 @@
+//
+//  GithubRepoSearchSettings.swift
+//  GithubDemo
+//
+//  Created by Swapnil Tamrakar on 2/14/17.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+import Foundation
+
+// Model class that represents the user's search settings
+class GithubRepoSearchSettings {
+    var searchString: String?
+    var minStars = 0
+    
+    init() {
+        
+    }
+}
